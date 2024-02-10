c21d7dd00501d243f3bb4dacd8d7e38b615a4510
Owen Delisle <Owen37164@gmail.com>
2024-02-10 12:02:30 -0800

    removing unused function

--- src/utils/math.ts
+++ src/utils/math.ts
@@ -1,14 +1,6 @@
 import type { GridPoint } from "../global_types/grid_point";
 
-export function inverse(number: number): number {
-	if (number !== 0) {
-		return 1 / number;
-	} else {
-		return 0
-	}
-}
-
-export function distance_between_points(point_one: GridPoint, point_two: GridPoint) {
+export function distance_between_points(point_one: GridPoint, point_two: GridPoint): number {
 	const delta_x = point_two.row - point_one.row - 1;
 	const delta_y = point_two.col - point_one.col - 1;
 
