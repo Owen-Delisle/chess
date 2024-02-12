537c69318a1fc952f47f21ddf3ee6dd927c00f12
Owen Delisle <Owen37164@gmail.com>
2024-02-11 20:36:46 -0800

    added tests to utility functions

--- tests/utils/arrays.test.ts
+++ tests/utils/arrays.test.ts
@@ -1,8 +1,31 @@
 import { arrays_are_equal } from "../../src/utils/arrays"
 
-const arr_1: number[] = [1,2,3]
-const arr_2: number[] = [1,2,3]
-
 test('checks arr_1 and arr_2 to be equal', () => {
-    expect(arrays_are_equal(arr_1,arr_2)).toBe(true);
+    const arr_1: number[] = [1,2,3]
+    const arr_2: number[] = [1,2,3]
+
+    const arr_3: string[] = ["1","2","3"]
+    const arr_4: string[] = ["1","2","3"]
+
+    const arr_5: string[] = []
+    const arr_6: string[] = []
+
+    expect(arrays_are_equal(arr_1,arr_2)).toBeTruthy();
+    expect(arrays_are_equal(arr_3,arr_4)).toBeTruthy();
+    expect(arrays_are_equal(arr_5,arr_6)).toBeTruthy();
+});
+
+test('checks arr_1 and arr_2 to not be equal', () => {
+    const arr_1: number[] = [1,2,3]
+    const arr_2: number[] = [3,2,1]
+
+    const arr_3: string[] = ["1","2","3"]
+    const arr_4: string[] = ["3","2","1"]
+
+    const arr_5: number[] = []
+    const arr_6: number[] = [NaN]
+
+    expect(arrays_are_equal(arr_1,arr_2)).toBeFalsy();
+    expect(arrays_are_equal(arr_3,arr_4)).toBeFalsy();
+    expect(arrays_are_equal(arr_5,arr_6)).toBeFalsy();
 });
--- tests/utils/math.test.ts
+++ tests/utils/math.test.ts
@@ -2,9 +2,23 @@ import type { GridPoint } from "../../src/global_types/grid_point"
 import { distance_between_points } from "../../src/utils/math"
 
 // Distance between points test
-const gp_1: GridPoint = {row: 0, col: 0}
-const gp_2: GridPoint = {row: 7, col: 7}
-
 test('checks distance_between_points to be equal', () => {
-    expect(distance_between_points(gp_1, gp_2)).toBe(8);
-});
+    const gp_1: GridPoint = {row: 0, col: 0}
+    const gp_2: GridPoint = {row: 7, col: 7}
+
+    expect(distance_between_points(gp_1, gp_2)).toBe(8)
+})
+
+test('checks distance_between_points with out of bounds coordinated to throw error', () => {
+    const gp_1: GridPoint = {row: -1, col: 0}
+    const gp_2: GridPoint = {row: 7, col: 7}
+    const gp_3: GridPoint = {row: NaN, col: 7}
+    const gp_4: GridPoint = {row: 7, col: NaN}
+    const gp_5: GridPoint = {row: -1, col: NaN}
+
+    expect(() => {distance_between_points(gp_1,gp_2) }).toThrow(`[object Object] is out of bounds.`);
+    expect(() => {distance_between_points(gp_2,gp_1) }).toThrow(`[object Object] is out of bounds.`);
+    expect(() => {distance_between_points(gp_2,gp_3) }).toThrow(`row input is NaN`);
+    expect(() => {distance_between_points(gp_2,gp_4) }).toThrow(`col input is NaN`);
+    expect(() => {distance_between_points(gp_2,gp_5) }).toThrow(`col input is NaN`);
+})
