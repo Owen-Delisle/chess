b6dca9f6ef8c606a7cff14a5d5ef6e1fb7382c70
Owen Delisle <Owen37164@gmail.com>
2024-02-27 17:14:39 -0800

    added hashing and secrets

--- server.ts
+++ server.ts
@@ -4,10 +4,12 @@ import { open } from 'sqlite';
 import { v4 as uuidv4 } from 'uuid';
 import jwt from 'jsonwebtoken'
 import path from 'path';
+require('dotenv').config();
+import bcrypt from 'bcrypt';
 
 const app = express();
 const PORT = 3000;
-const secretKey = 'your_secret_key';
+const secretKey = process.env.JWT_SECRET;
 
 const dbPromise = open({
     filename: './database/database.db',
@@ -35,7 +37,13 @@ app.post('/signup', async (req, res) => {
     const id = uuidv4()
     try {
         const db = await dbPromise;
-        await db.run('INSERT INTO users (id, username, email, password) VALUES (?, ?, ?, ?)', [id, username, email, password]);
+		const saltRounds = 10;
+        const salt = await bcrypt.genSalt(saltRounds);
+
+		console.log("Password:", password, "Salt:", salt)
+		const hashedPassword = await bcrypt.hash(password, salt);
+
+        await db.run('INSERT INTO users (id, username, email, password) VALUES (?, ?, ?, ?)', [id, username, email, hashedPassword]);
         
         res.send('User signed up successfully!');
     } catch (error) {
@@ -50,13 +58,22 @@ app.post('/login', async (req, res) => {
     try {
         // Query database to check if user exists
         const db = await dbPromise;
-        const user = await db.get('SELECT * FROM users WHERE username = ? AND password = ?', [username, password]);
+        const user = await db.get('SELECT * FROM users WHERE username = ?', [username]);
 
         if (user) {
-			const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
-            res.json({ token });
+			const passwordMatch = await bcrypt.compare(password, user.password);
+			if(passwordMatch) {
+				if(secretKey !== undefined) {
+					const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
+					res.json({ token });
+				} else {
+					throw new Error("Secret Key was undefined")
+				}
+			} else {
+				res.status(401).send('Invalid Password')
+			}
         } else {
-            res.status(401).send('Invalid username or password');
+            res.status(401).send('Invalid Username');
         }
     } catch (error) {
         console.error('Error logging in user:', error);
